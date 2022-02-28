5f10f460bfbec0f8a628eb63a4378680452942c4
Gert Adamson <adamson.gert@hotmail.com>
2022-02-28 20:33:42 +0200

    create SumAllTails function

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,19 @@
 package main
 
-func Sum(numbers []int) int {
-	sum := 0
+func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
-	return sum
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return
 }
