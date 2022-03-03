ae77f0e42b39007e2afddbe8f1bad3a1d452540f
Gert Adamson <adamson.gert@hotmail.com>
2022-03-03 07:31:03 +0200

    return error if word not found in dictionary

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,7 +1,17 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(term string) string {
-	return d[term]
+var ErrNotFound = errors.New("Could not find the word searched for.")
+
+func (d Dictionary) Search(term string) (string, error) {
+	definition, found := d[term]
+
+	if !found {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
