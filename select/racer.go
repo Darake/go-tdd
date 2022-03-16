645c96db63e444ec6b4a92f26b4ebfd0fd29a8f5
Gert Adamson <adamson.gert@hotmail.com>
2022-03-16 09:21:40 +0200

    create url racer

---
+++ select/racer.go
@@ -0,0 +1,23 @@
+package racer
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) string {
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
+
+	if aDuration > bDuration {
+		return b
+	}
+
+	return a
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
